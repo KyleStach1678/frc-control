37f8cc0aaae173507aef7c5a0e7992b227c19851
Kylestach1678 <Kylestach99@gmail.com>
2015-09-07 15:13:58 -0700

    Added state space controller

---
+++ src/Control/StateSpaceController.h
@@ -0,0 +1,67 @@
+/*
+ * StateSpaceController.h
+ *
+ *  Created on: Sep 6, 2015
+ *      Author: Kyle
+ */
+
+#ifndef SRC_CONTROL_STATESPACECONTROLLER_H_
+#define SRC_CONTROL_STATESPACECONTROLLER_H_
+
+#include <Control/Controller.h>
+#include <Utils/Matrix.h>
+
+namespace Citrus
+{
+
+template <int SensorSpace, int StateSpace, int OutputSpace>
+class StateSpaceController : public Controller<Vector<SensorSpace>, Vector<OutputSpace>>
+{
+	Matrix<StateSpace, StateSpace> A;
+	Matrix<StateSpace, OutputSpace> B;
+	Matrix<SensorSpace, StateSpace> C;
+
+	Matrix<OutputSpace, StateSpace> K;
+	Matrix<StateSpace, InputSpace> L;
+
+	Vector<StateSpace> x_hat;
+	Vector<StateSpace> goal;
+
+  public:
+	StateSpaceController(ControlInput<Vector<SensorSpace>>& input, ControlOutput<Vector<OutputSpace>>& output) : Controller(input, output)
+	{
+	}
+
+	void SetGoal(Vector<StateSpace> goal)
+	{
+		this->goal = goal;
+	}
+
+	void SetGains(Matrix<StateSpace, StateSpace> A,
+				  Matrix<StateSpace, OutputSpace> B,
+				  Matrix<SensorSpace, StateSpace> C,
+				  Matrix<OutputSpace, StateSpace> K,
+				  Matrix<StateSpace, InputSpace> L)
+	{
+		this->A = A;
+		this->B = B;
+		this->C = C;
+		this->K = K;
+		this->L = L;
+	}
+
+	Vector<OutputSpace> Calculate(Vector<InputSpace> input, Time dt)
+	{
+		Vector<StateSpace> error = goal - input;
+		Vector<OutputSpace> output = K * error;
+		Vector<InputSpace> y_hat = C * x_hat;
+		x_hat = x_hat + (A * x_hat + B * output) * dt + l * (input - y_hat);
+		return output;
+	}
+
+	virtual ~StateSpaceController();
+};
+
+} /* namespace Citrus */
+
+#endif /* SRC_CONTROL_STATESPACECONTROLLER_H_ */
